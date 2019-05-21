b03b340ee9882f68c8cc17ea3361fa8f17653e0c
cameron <tho17015@byui.edu>
2019-05-20 19:37:54 -0600

    added github grabber

--- scripts.js
+++ scripts.js
@@ -1,10 +1,28 @@
-const request = require('request');
+var url = 'https://api.github.com/users/cdthomp1/repos?sort=updated'
 
-var apiKey = "534bcbe72b02aef5fe56b07267f4c41c";
-var city = "tucson"; // My test case was "London"
-var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-request(url, function(err, resonse, body){
-    if (err) {console.log(err)}
-    else { console.log(body)}
-})
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        var github = JSON.parse(this.responseText)
+        var i = 1;
+        github.forEach(repo => {
+            console.log(repo.name)
+            document.getElementById("name" + i).innerHTML = repo.name;
+            document.getElementById("lang" + i).innerHTML = repo.language;
+            
+            var repoLang = repo.languages_url;
+            i++;
+        });
+
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+
+
+
+
+  loadDoc()
