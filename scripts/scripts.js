84debbf5c9624eeb35cef9f681152218991035ad
cdthomp1 <tho17015@byui.edu>
2020-03-02 22:06:10 -0700

    styling edits

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,121 +1,121 @@
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 33,
-        "density": {
-          "enable": true,
-          "value_area": 1420.4657549380909
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "triangle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.06313181133058181,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 11.83721462448409,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
+  "particles": {
+    "number": {
+      "value": 33,
+      "density": {
         "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "value_area": 1420.4657549380909
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "triangle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
-    "retina_detect": true
-  });
-   
+    "opacity": {
+      "value": 0.06313181133058181,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 11.83721462448409,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
 var githubURL = 'https://api.github.com/users/cdthomp1/repos?sort=updated'
 
 
 async function getGithub() {
   return await fetch(githubURL).then(res => {
     return res.json();
-  }).catch((e)=>{
+  }).catch((e) => {
     console.error(e)
   })
 }
@@ -123,59 +123,81 @@ async function getGithub() {
 
 function displayResults(repo) {
 
-    var div = document.createElement("div");
-    div.classList.add("githubProject");
-    var repoHeader = repo.name;
-   
-    if (repo.language === null) {
-      var repoLanguage = "None Yet!"
-    } else {
-      var repoLanguage = repo.language;
-    }
+  var div = document.createElement("div");
+  div.classList.add("githubProject");
+  var repoHeader = repo.name;
 
-    if (repo.description === null) {
-      var repoDescription = "No Description Yet!"
-    } else {
-      var repoDescription = repo.description;
-    }
+  if (repo.language === null) {
+    var repoLanguage = "None Yet!"
+  } else {
+    var repoLanguage = repo.language;
+  }
+
+  if (repo.description === null) {
+    var repoDescription = "No Description Yet!"
+  } else {
+    var repoDescription = repo.description;
+  }
 
 
-    var projectHeader = document.createElement("h3");
-    projectHeader.setAttribute("class", "projectHeader")
-    var projectLink = document.createElement("a");
-    projectLink.innerText = repoHeader;
-    projectLink.setAttribute("href", repo.html_url);
-    projectLink.setAttribute("target", "_blank");
-    projectHeader.appendChild(projectLink);
-    div.appendChild(projectHeader);
-    var p = document.createElement("p");
-    var languageTextnode = document.createTextNode("Language: " + repoLanguage);
-    p.appendChild(languageTextnode);
-    div.appendChild(p);
 
-    var pD = document.createElement("p");
-    var descriptionTextnode = document.createTextNode(repoDescription);
-    pD.appendChild(descriptionTextnode);
-    div.appendChild(pD);
 
-    
-    if (document.getElementById("githubProjectsHome") !== null) {
-      document.getElementById("githubProjectsHome").appendChild(div)
-    } else {
-      document.getElementById("githubProjects").appendChild(div)
-    }
+
+  var projectHeader = document.createElement("h3");
+  projectHeader.setAttribute("class", "projectHeader")
+  var projectLink = document.createElement("a");
+  projectLink.innerText = repoHeader;
+  projectLink.setAttribute("href", repo.html_url);
+  projectLink.setAttribute("target", "_blank");
+  projectHeader.appendChild(projectLink);
+  div.appendChild(projectHeader);
+  var p = document.createElement("p");
+  var languageTextnode = document.createTextNode("Language: " + repoLanguage);
+  p.appendChild(languageTextnode);
+  div.appendChild(p);
+
+  var pD = document.createElement("p");
+  var descriptionTextnode = document.createTextNode(repoDescription);
+  pD.appendChild(descriptionTextnode);
+  div.appendChild(pD);
+
+
+  
+
+  if (repo.homepage === null) {
+    var liveLink = document.createElement("p");
+    liveLink.innerText = `No Live link yet.`;
+  } else {
+    var liveLink = document.createElement("a");
+    // var br = document.createElement("br"); WORK ON ADDING A BREAK
+    liveLink.innerText = `View Project Live: ${repo.homepage}`;
+    liveLink.setAttribute('href', repo.homepage);
+    liveLink.setAttribute("target", "_blank");
+  }
+
+
+
+
+  div.appendChild(liveLink);
+
+
+  if (document.getElementById("githubProjectsHome") !== null) {
+    document.getElementById("githubProjectsHome").appendChild(div)
+  } else {
+    document.getElementById("githubProjects").appendChild(div)
+  }
 
 }
 
 getGithub().then(res => {
   if (document.getElementById("githubProjectsHome") !== null) {
-    var repos = res.slice(0,3)
+    var repos = res.slice(0, 3)
   } else {
     var repos = res
   }
   repos.forEach(repo => {
     displayResults(repo);
   })
-}).catch((e)=>{
+}).catch((e) => {
   console.error(e)
 })
