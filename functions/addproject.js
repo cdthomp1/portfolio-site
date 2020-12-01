b413b791f30fcef6a0b1bc9edd1983283248e8f0
Cameron Thompson <cameronthompson@Camerons-MacBook-Pro.local>
2020-12-01 01:51:59 -0700

    update function

--- functions/addproject.js
+++ functions/addproject.js
@@ -1,4 +1,5 @@
 exports.handler = async function (event, context) {
+    console.log("hello");
     return {
         statusCode: 200,
         body: JSON.stringify({ event: event, context: context })
