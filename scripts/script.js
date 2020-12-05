e8b4997467ed0ce49a40103be030cf24cb3e6c23
Cameron Thompson <cameronthompson@Camerons-MacBook-Pro.local>
2020-12-04 23:56:17 -0700

    added articles

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,7 @@ async function displayProjects() {
 	var tempDiv = document.getElementById('projects');
 
 	projects.forEach(project => {
-		console.log(project)
+		// console.log(project)
 		var htmlStuff = `
 		<div class="wrapper">
 			<div class="dots-wrapper card-header">
@@ -51,4 +51,44 @@ async function displayProjects() {
 	})
 }
 
+async function displayArticles() {
+	const articles = await fetch('https://dev.to/api/articles?username=cdthomp1')
+		.then(response => response.json())
+		.then(json => {
+			return json
+		});
+
+	var articleDiv = document.getElementById('articles');
+
+	articles.forEach(article => {
+		console.log(article)
+		var articleHtml = `
+		<div class="wrapper">
+			<div class="dots-wrapper card-header">
+				<div id="dot-1" class="browser-dot"></div>
+				<div id="dot-2" class="browser-dot"></div>
+				<div id="dot-3" class="browser-dot"></div>
+			</div>
+			<div class="post">
+				<img class="thumbnail" src="${article.cover_image}" alt="image of NPM card">
+				<div class="post-preview">
+					<h6 class="post-title">${article.title}</h6>
+					<p class="post-intro">${article.description}</p>
+					<div class="link-container">
+						<a class="read-more" target="_blank"
+							href="${article.canonical_url}">Read Article</a>
+						</div>
+					</div>
+				</div>
+			</div>`
+		
+
+
+
+		articleDiv.insertAdjacentHTML('beforeend', articleHtml);
+
+	})
+}
+
 displayProjects();
+displayArticles();
