26fdd8a5210f39d8ff98653d7bf2650100309bbe
cdthomp1 <camthomp96@gmail.com>
2021-04-13 18:25:07 -0700

    Nav dev

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,25 +2,20 @@ import React from 'react'
 
 const Navigation = () => {
     return (
-        <>
+        <nav>
             <div className="nav-wrapper">
-                <a href="/">
-                    <div className="dots-wrapper">
-                        <div id="dot-1" className="browser-dot"></div>
-                        <div id="dot-2" className="browser-dot"></div>
-                        <div id="dot-3" className="browser-dot"></div>
-                    </div>
-                </a>
+                <div>
+                    <a href="/"><img className="logo" src="/images/Dev.png" alt="Logo" /></a>
+                </div>
 
-                <ul id="navigation">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/projects">Projects</a></li>
-                    <li><a href="/articles">Articles</a></li>
-                    <li><a href="/contact">Contact</a></li>
-
-                </ul>
+                <div id="navigation">
+                    <a href="/">Home</a>
+                    <a href="/projects">Projects</a>
+                    <a href="/articles">Articles</a>
+                    <a href="/contact">Contact</a>
+                </div>
             </div>
-        </>
+        </nav>
     )
 }
 
