9bc05af86159b948607c570c9f156892a108cd10
cdthomp1 <camthomp96@gmail.com>
2021-04-13 18:25:16 -0700

    Footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,14 @@ const FooterScreen = () => {
         <>
             <footer>
                 <img id="footerImg" alt="logo" src="/images/Dev.png" />
+                <p>&copy; Cameron Thompson 2021</p>
                 <ul className="footerLinks">
                     <li><a href="https://www.linkedin.com/in/cameron-thompson96/" target="_blank"
-                        rel="noopener noreferrer">Linkedin</a></li>
+                        rel="noopener noreferrer">LinkedIn</a></li>
                     <li><a href="https://github.com/cdthomp1" target="_blank" rel="noopener noreferrer">Github</a></li>
-
                     <li><a href="/contact">Contact</a></li>
+                    <li><a href="https://twitter.com/DeveloperCam" target="_blank"
+                        rel="noopener noreferrer">Twitter</a></li>
                 </ul>
             </footer>
         </>
