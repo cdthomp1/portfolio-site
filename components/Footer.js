29a4f11b1b31ef30ee66a22677abb0621df29c30
cdthomp1 <camthomp96@gmail.com>
2021-07-22 22:15:50 -0600

    linkedin link fix

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <img id="footerImg" alt="logo" src="/images/Dev.png" />
                 <p>&copy; Cameron Thompson 2021</p>
                 <ul className="footerLinks">
-                    <li><a href="https://www.linkedin.com/in/cameron-thompson96/" target="_blank"
+                    <li><a href="https://www.linkedin.com/in/cameron-thompson-dev/" target="_blank"
                         rel="noopener noreferrer">LinkedIn</a></li>
                     <li><a href="https://github.com/cdthomp1" target="_blank" rel="noopener noreferrer">Github</a></li>
                     <li><a href="/construction">Contact</a></li>
